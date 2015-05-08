a3b0fe90032823bfd8cb820e94a1c04ca39ca6d0
mordeth <cvetanov@gmail.com>
2015-05-08 22:55:43 +0300

    Update DB sql dump

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2015 at 07:33 PM
+-- Generation Time: May 08, 2015 at 09:55 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -44,9 +44,18 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) NOT NULL,
   `title` varchar(200) CHARACTER SET utf16 NOT NULL,
   `content` text CHARACTER SET utf8 NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `author` int(11) NOT NULL,
   `views` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `date`, `author`, `views`) VALUES
+(3, 'Aldus PageMaker including', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2015-05-08 19:41:18', 1, 0),
+(6, 'It is a long established fact that a reader will be distracted', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', '2015-05-08 19:43:19', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -57,8 +66,18 @@ CREATE TABLE IF NOT EXISTS `posts` (
 CREATE TABLE IF NOT EXISTS `tags` (
   `id` int(11) NOT NULL,
   `title` varchar(200) CHARACTER SET utf8 NOT NULL,
-  `post` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `title`, `post_id`) VALUES
+(7, 'blog', 1),
+(8, 'post', 1),
+(9, 'mvc', 1),
+(10, 'softuni', 1);
 
 -- --------------------------------------------------------
 
@@ -70,17 +89,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `is_author` tinyint(1) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `name`) VALUES
-(1, 'mordeth', '96e79218965eb72c92a549dd5a330112', 'Cvetan Cvetanov'),
-(5, 'alerzhus', '698d51a19d8a121ce581499d7b701668', 'Alerzhus'),
-(7, 'asd', '698d51a19d8a121ce581499d7b701668', 'asds');
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `is_author`, `is_admin`) VALUES
+(1, 'mordeth', '96e79218965eb72c92a549dd5a330112', 'Cvetan Cvetanov', '', 1, 1),
+(5, 'alerzhus', '698d51a19d8a121ce581499d7b701668', 'Alerzhus', '', 0, 0),
+(7, 'asd', '698d51a19d8a121ce581499d7b701668', 'asds', 'cvetanov@gmail.com', 0, 0),
+(8, 'dsa', '698d51a19d8a121ce581499d7b701668', 'dsa', 'dsa@abv.bg', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -123,17 +146,17 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
